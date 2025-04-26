6a69f6a1a57d9d9df769676d7ef21d04956bc415
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-04-26 21:45:47 +0530

    custom user auth

--- src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
@@ -1,12 +1,20 @@
 package com.luv2code.jobportal.config;
 
+import com.luv2code.jobportal.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
 
     private final String[] publicUrl = {"/",
             "/global-search/**",
@@ -23,8 +31,15 @@ public class WebSecurityConfig {
             "/*.js.map",
             "/fonts**", "/favicon.ico", "/resources/**", "/error"};
 
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authenticationProvider(authenticationProvider());
 
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers(publicUrl).permitAll();
@@ -32,5 +47,23 @@ public class WebSecurityConfig {
         });
 
         return http.build();
+    }
+
+//    this method will tell spring security how to find our users and how to authenticate passwords
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+//above line tells -> how to get the users
+
+        return authenticationProvider;
+    }
+
+//    how to authenticate passwords, plain text or encryption
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
